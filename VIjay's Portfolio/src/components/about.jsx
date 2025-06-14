2b87205caa91e5f7ff9e27a1c44c04ba4156aaff
vijaycb007 <vijaycbcoorg@gmail.com>
2025-06-14 21:01:20 +0530

    Update about.jsx

--- VIjay's Portfolio/src/components/about.jsx
+++ VIjay's Portfolio/src/components/about.jsx
@@ -68,7 +68,7 @@ const About = () => {
             world of web development.
           </p>
           <a
-            href="https://drive.google.com/file/d/17Ihx00fgLrWs3yBLEf6P52sg-yGT6Huk/view?usp=sharing"
+            href="https://drive.google.com/file/d/1LdIqrKLVZr1wormRQvhoe9Yw4eIyTYyQ/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
