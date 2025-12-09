f5d67cccde1c9d0c892de3dd4fd475d60be36c6f
vijaycb007 <vijaycbcoorg@gmail.com>
2025-12-09 10:32:07 +0530

    Update constants.js

--- VIjay's Portfolio/src/constants.js
+++ VIjay's Portfolio/src/constants.js
@@ -236,7 +236,8 @@ export const experiences = [
       desc: "Joined as a trainee in Jspiders as a Java FullStack developer.",
       skills: [
         "SQL",
-        "Java"
+        "Java",
+        "HTML"
       ],
     link: "",
     },
@@ -363,6 +364,7 @@ export const experiences = [
       webapp: "https://www.figma.com/proto/5efPsg6vQmwjHiSs5fQBWQ/Buzzly?node-id=0-1&t=f6XlbP8uM6nBdZ8f-1",
     },
   ];
+
 
 
 
