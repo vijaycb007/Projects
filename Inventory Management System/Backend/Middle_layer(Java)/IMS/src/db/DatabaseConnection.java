f77ec5797341f2d7985d3c3cb9c1c0110cd6083d
vijaycb007 <vijaycbcoorg@gmail.com>
2025-12-09 16:22:05 +0530

    Update DatabaseConnection.java

--- Inventory Management System/Backend/Middle_layer(Java)/IMS/src/db/DatabaseConnection.java
+++ Inventory Management System/Backend/Middle_layer(Java)/IMS/src/db/DatabaseConnection.java
@@ -6,11 +6,10 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String URL = "jdbc:postgresql://localhost:5432/Inventory_db";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "12345";
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
-    // Method to build connection with PostgreSQL
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
