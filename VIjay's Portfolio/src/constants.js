a66693bdf1271be9cccdee4b246fcdf558ceafa3
Vijay C B <vijaycbcoorg@gmail.com>
2026-01-06 14:11:01 +0530

    Update constants.js

--- VIjay's Portfolio/src/constants.js
+++ VIjay's Portfolio/src/constants.js
@@ -16,6 +16,7 @@ import javaLogo from './assets/tech_logo/java_logo.jpg';
 import postgreSQLLogo from './assets/tech_logo/Postgresql_logo.svg';
 import MySQLLogo from './assets/tech_logo/MySQL_logo.png';
 import EclipseLogo from './assets/tech_logo/eclipse_logo.png';
+import turboCLogo from './assets/tech_logo/turboC.png';
 
 // Experience Section Logo's
 import Rablologo from './assets/company_logo/Rablologo.png';
@@ -72,6 +73,7 @@ export const SkillsInfo = [
       { name: 'GitHub', logo: githubLogo },
       { name: 'VS Code', logo: vscodeLogo },
       { name: 'Netlify', logo: netlifyLogo },
+      { name: 'Turbo C', logo: turboCLogo },
       { name: 'Figma', logo: figmaLogo },
       { name: 'Jira', logo: jiraLogo },
       { name: 'Vercel', logo: vercelLogo },
@@ -343,6 +345,7 @@ export const experiences = [
       webapp: "https://www.figma.com/proto/5efPsg6vQmwjHiSs5fQBWQ/Buzzly?node-id=0-1&t=f6XlbP8uM6nBdZ8f-1",
     },
   ];
+
 
 
 
